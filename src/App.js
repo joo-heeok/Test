bb0f7e1b007d1b23f5cc7ba94582df145cd76dfa
옥주희 <jooheeok92@gmail.com>
2023-04-09 22:39:29 +0900

    [Feat] 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./pages/Header";
+import { Main } from "./pages/Main";
+import { NotFound } from "./pages/NotFound.jsx";
+import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
+import { Todo } from "./pages/Todo";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/todo" element={<Todo />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export function signIn() {
+export function SignIn() {
   return <div>SignIn</div>;
 }
